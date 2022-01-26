068dc05a32e1811f66748c015946e28e566cb080
génie-faire <jennifer.cavaccuiti@yahoo.fr>
2022-01-26 11:24:26 +0000

    WIP : react admin

--- my-admin/src/App.js
+++ my-admin/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { HydraAdmin } from "@api-platform/admin";
 
-export default App;
+// Replace with your own API entrypoint
+// For instance if https://example.com/api/books is the path to the collection of book resources, then the entrypoint is https://example.com/api
+export default () => (
+    <HydraAdmin entrypoint="http://res.localhost/api/" />
+);
