8ea0de1e65527da19e6375ab3a5445abca651455
DUSSARTLuke <lukedussart@hotmail.fr>
2022-01-28 16:56:02 +0100

    finition inscription partielle

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Routes, Route, Link} from "react-router-dom";
 import Home from "./Home";
 import Connexion from "./Connexion";
