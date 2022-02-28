6dbe1c15eda982d092f7cd3611eb5f02658d56b0
DUSSARTLuke <lukedussart@hotmail.fr>
2022-02-28 16:16:51 +0100

    ajout de la fonctionnaités pwa sans avoir le https

---
+++ public/serviceWorker.js
@@ -0,0 +1,55 @@
+
+const staticCacheName = "cache-v1";
+const assets = ["/", "/index.php"];
+
+self.addEventListener('install', (e) => {
+   e.waitUntil(
+       caches.open(staticCacheName).then((cache) => {
+           cache.addAll(assets)
+       })
+   );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            // IMPORTANT: Cloner la requête.
+            // Une requete est un flux et est à consommation unique
+            // Il est donc nécessaire de copier la requete pour pouvoir l'utiliser et la servir
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(function (response) {
+                if (!response || response.status !== 200 || response.type !== "basic") {
+                    return response;
+                }
+
+                // IMPORTANT: Même constat qu'au dessus, mais pour la mettre en cache
+                var responseToCache = response.clone();
+
+                caches.open(staticCacheName).then(function (cache) {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
+        })
+    );
+});
+
+// supprimer caches
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.add(
+                keys
+                    .filter((key) => key !== staticCacheName)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
