6393caabf458282dc57e387e6071250cd5a8c887
DUSSARTLuke <lukedussart@hotmail.fr>
2022-03-01 16:17:19 +0100

    filtrage essai + card

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,28 @@ if("serviceWorker" in navigator){
     navigator.serviceWorker.register("../serviceWorker.js");
 }
 
+const filter = document.querySelectorAll('.block-action.filter');
+
+if (filter) {
+    filter.forEach((menu) => {
+        console.log(menu);
+        const buttons = menu.querySelectorAll('.filter-tag');
+        // console.log(buttons);
+        buttons.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                buttons.forEach((item) => {
+                    item.classList.remove('active');
+                });
+                e.target.classList.add('active');
+                console.log('ok');
+                document.querySelectorAll(e.target.dataset.all).forEach((item) => {
+                    item.classList.add('d-none');
+                });
+
+                document.querySelectorAll(e.target.dataset.target).forEach((item) => {
+                    item.classList.remove('d-none');
+                })
+            })
+        })
+    })
+}
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\RelationTypeRepository;
 use App\Repository\ResourceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,18 +23,30 @@ class IndexController extends AbstractController
      * )
      * @Rest\Route(path="/", name="home")
      */
-    public function home(ResourceRepository $resourceRepository, CategoryRepository $categoryRepository)
+    public function home(ResourceRepository $resourceRepository, CategoryRepository $categoryRepository, RelationTypeRepository $relationRepo)
     {
         $resources = $resourceRepository->findBy([
             'status' => 'PU'
         ]);
 
-
+        $relations = $relationRepo->findAll();
         $categories = $categoryRepository->categoriesWithPublishedResources();
+        $resourceTypes = [
+            'GA' =>'Jeu à réaliser / activité',
+            'AR' => 'Article' ,
+            'CH' =>'Carte défi',
+            'PC' => 'Cours au format PDF',
+            'WO' => 'Exercice / atelier',
+            'RS' => 'Fiche de lecture',
+            'OG' => 'Jeu en ligne',
+            'VI' => 'Vidéo'
+        ];
 
         return $this->render("pages/index/index.html.twig", [
             'resources' => $resources,
-            'categories' => $categories
+            'categories' => $categories,
+            'relations' => $relations,
+            'resourceTypes' => $resourceTypes
         ]);
     }
 }
