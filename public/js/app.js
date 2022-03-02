03c7b9faa54195dafd9a2b2795cdf6689d43c955
DUSSARTLuke <lukedussart@hotmail.fr>
2022-03-02 09:15:19 +0100

    ajout changement pour navbar et card

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("../serviceWorker.js");
-}
+// if("serviceWorker" in navigator){
+//     navigator.serviceWorker.register("../serviceWorker.js");
+// }
 
 const filter = document.querySelectorAll('.block-action.filter');
 
