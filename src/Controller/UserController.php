fb1840918cbb65100a4e5c5e88f452bbe1dea012
DUSSARTLuke <lukedussart@hotmail.fr>
2022-04-29 11:11:43 +0200

    modification de profil fonctionne + modif navbar

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/profil/", name="profil_")
@@ -17,19 +18,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/edit/{id}/", name="edit")
+     * @Route("view/{id}/", name="view")
      */
-    public function editRessources(Request $request, EntityManagerInterface $manager, User $user): Response
+    public function voirProfil(EntityManagerInterface $manager, User $user): Response
+    {
+        return $this->render('pages/user/view.html.twig', ["user" => $user]);
+    }
+
+
+    /**
+     * @Route("edit/{id}/", name="edit")
+     */
+    public function editProfil(Request $request, EntityManagerInterface $manager, User $user, UserPasswordEncoderInterface $passwordEncoder,): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $pwd = $form->getData()->getPassword();
+            $confPwd = $form->getData()->getConfPassword();
+            if (($pwd !== '' and $confPwd !== '') and ($pwd === $confPwd)) {
+                $user->setPassword($passwordEncoder->encodePassword($user, $pwd));
+                $user->setconfPassword($passwordEncoder->encodePassword($user, $confPwd));
+            } else {
+                $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
+                $user->setconfPassword($passwordEncoder->encodePassword($user, $user->getConfPassword()));
+            }
             $manager->persist($user);
             $manager->flush();
 
-            $this->addFlash('success', " Votre ressource a bien été modifiée !");
+            $this->addFlash('success', " Votre profil a bien été modifié !");
 
-            return $this->redirectToRoute('ressources_consult', ['id' => $user->getId()]);
         }
 
         return $this->render('pages/user/form.html.twig', ['form' => $form->createView(), 'from' => 'edit']);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -22,7 +22,9 @@ class UserType extends AbstractType
             ->add('username', TextType::class)
             ->add('email', EmailType::class)
             ->add('address1', TextType::class)
-            ->add('address2', TextType::class)
+            ->add('address2', TextType::class, [
+                'required' => false
+            ])
             ->add('postalCode', NumberType::class)
             ->add('city', TextType::class)
             ->add('birthday', DateTimeType::class, [
@@ -30,13 +32,28 @@ class UserType extends AbstractType
             ])
             ->add('socialSituation', ChoiceType::class, [
                 'choices'  => [
-                    'Ouvrier' => 'Ouvrier',
+                    "Aucune réponse" => "",
+                    'Agriculteur exploitant' => 'Agriculteur exploitant',
+                    "Artisan, commerçant ou chef d'entreprise" => "Artisan, commerçant ou chef d'entreprise",
+                    "Cadre et Professions intellectuelles supérieures" => "Cadre et Professions intellectuelles supérieures",
+                    "Professions intermédiaires" => "Professions intermédiaires",
                     'Employé non qualifié' => 'Employé non qualifié',
                     'Employé qualifié' => 'Employé qualifié',
+                    "Étudiant" => "Étudiant",
+                    'Ouvrier' => 'Ouvrier',
+                    "Retraité" => "Retraité",
+                    "Sans activité professionnelle" => "Sans activité professionnelle"
                 ],
+                "required" => false
             ])
-            ->add('password', PasswordType::class)
-            ->add('confPassword', PasswordType::class)
+            ->add('password', PasswordType::class, [
+                'required' => false,
+                'empty_data' => '',
+            ])
+            ->add('confPassword', PasswordType::class, [
+                'required' => false,
+                'empty_data' => '',
+            ])
             ->add('isRGPD', CheckboxType::class, [
                 'label'    => 'Acceptez-vous que vos données soient collectées par le ministère?'])
         ;
